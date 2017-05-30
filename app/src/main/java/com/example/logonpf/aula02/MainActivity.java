12a91f6ddd2b94a2170ccea02c4509fd75aabf59
logonpf <logonpf@fiap.com.br>
2017-05-29 21:22:36 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonpf/aula02/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.logonpf.aula02;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextInputLayout tilNome;
+    private TextInputLayout tilIdade;
+    private Spinner spTimes;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tilNome = (TextInputLayout) findViewById(R.id.tilNome);
+        tilIdade = (TextInputLayout) findViewById(R.id.tilIdade);
+        spTimes = (Spinner) findViewById(R.id.spTimes);
+    }
+
+    public void cadastrar(View v){
+        Intent intencaoConfirmar = new Intent(this,
+                ConfirmacaoActivity.class);
+
+        intencaoConfirmar.putExtra("NOME",
+                tilNome.getEditText().getText().toString());
+
+        intencaoConfirmar.putExtra("IDADE",
+                tilIdade.getEditText().getText().toString());
+
+        intencaoConfirmar.putExtra("TIME",
+                spTimes.getSelectedItem().toString());
+
+        startActivity(intencaoConfirmar);
+    }
+}
